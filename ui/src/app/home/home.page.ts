5565880d876423f36f6bb511d1c17253ef38344a
galwort <wthomasgorbett@gmail.com>
2024-01-19 20:51:15 -0500

    When food is submitted, return the nutrients

--- ui/src/app/home/home.page.ts
+++ ui/src/app/home/home.page.ts
@@ -8,16 +8,21 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePage {
   meal: string = '';
+  mealSubmitted: boolean = false;
+  nutrients: any = null;
 
   constructor(private http: HttpClient) {}
 
   submitMeal() {
-    const nutrients = {
-      carbs: 50,
-      fats: 20,
-      proteins: 30,
-      calories: 400
-    };
+    setTimeout(() => {
+      this.nutrients = {
+        carbs: 50,
+        fats: 20,
+        proteins: 30,
+        calories: 400
+      };
+      this.mealSubmitted = true;
+    }, 1000);
     // this.http.post<any>('http://localhost:8000/process_meal/', { text: this.meal })
     //   .subscribe({
     //     next: (response) => {
