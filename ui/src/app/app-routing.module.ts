fddd8cdf8c2c775aa81a492d1655b4e25af753d3
galwort <wthomasgorbett@gmail.com>
2024-09-05 17:08:10 -0400

    Generate login page

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -6,7 +6,11 @@ const routes: Routes = [
     path: '',
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomePageModule),
+  },  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+
 ];
 
 @NgModule({
