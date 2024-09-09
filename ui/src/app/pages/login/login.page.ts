b5d102be66b352705b2e107a2fd3eaec3438704b
galwort <wthomasgorbett@gmail.com>
2024-09-09 14:15:20 -0400

    Updates

--- ui/src/app/pages/login/login.page.ts
+++ ui/src/app/pages/login/login.page.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
   constructor(private router: Router) {}
 
-  ngOnInit() {}
+  googleLogin() {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
 
-  navigateTo(page: string) {
-    this.router.navigate([`/${page}`]);
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        this.router.navigate(['/home']);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
