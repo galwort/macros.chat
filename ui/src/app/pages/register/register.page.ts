360b908176727d5977338680694257b6c18b6ab3
galwort <wthomasgorbett@gmail.com>
2024-09-24 18:32:20 -0400

    Match register styling to login

--- ui/src/app/pages/register/register.page.ts
+++ ui/src/app/pages/register/register.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { environment } from 'src/environments/environment';
+
+export const app = initializeApp(environment.firebaseConfig);
+export const db = getFirestore(app);
 
 @Component({
   selector: 'app-register',
@@ -7,9 +13,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  meals: any[] = [];
+
   constructor(private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loadMeals();
+  }
+
+  async loadMeals() {
+    const mealCollection = collection(db, 'meals');
+    const mealSnapshot = await getDocs(mealCollection);
+    this.meals = mealSnapshot.docs.map((doc) => doc.data());
+    this.shuffleArray(this.meals);
+  }
+
+  shuffleArray(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  getColorClass(index: number): string {
+    const colorClasses = ['color-one', 'color-two', 'color-three'];
+    return colorClasses[index % 3];
+  }
 
   navigateTo(page: string) {
     this.router.navigate([`/${page}`]);
