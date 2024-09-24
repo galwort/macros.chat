29df969c3a9d8d20d41536f84cb1566853ea430e
galwort <wthomasgorbett@gmail.com>
2024-09-24 16:22:08 -0400

    Bring in meals for login design

--- ui/src/app/pages/login/login.page.ts
+++ ui/src/app/pages/login/login.page.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { environment } from 'src/environments/environment';
+
+export const app = initializeApp(environment.firebaseConfig);
+export const db = getFirestore(app);
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage {
+export class LoginPage implements OnInit {
+  meals: any[] = [];
+
   constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.loadMeals();
+  }
+
+  async loadMeals() {
+    const mealCollection = collection(db, 'meals');
+    const mealSnapshot = await getDocs(mealCollection);
+    this.meals = mealSnapshot.docs.map((doc) => doc.data());
+  }
+
+  getColorClass(index: number): string {
+    const colorClasses = ['color-one', 'color-two', 'color-three'];
+    return colorClasses[index % 3];
+  }
 
   googleLogin() {
     const auth = getAuth();
