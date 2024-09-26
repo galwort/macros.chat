937c0404766da040ef1eadfc6bda2ff9045222e5
galwort <wthomasgorbett@gmail.com>
2024-09-26 14:13:14 -0400

    Copy over skeleton styling

--- ui/src/app/pages/journal/journal.page.ts
+++ ui/src/app/pages/journal/journal.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-journal',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./journal.page.scss'],
 })
 export class JournalPage implements OnInit {
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  navigateTo(page: string) {
+    this.router.navigateByUrl(`/${page}`);
   }
-
 }
