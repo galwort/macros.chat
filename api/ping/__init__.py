b0c637062ce42b93fb654a39103adf72bd4cfb8c
galwort <wthomasgorbett@gmail.com>
2024-10-07 17:12:01 -0400

    Remove schedule from init

--- api/ping/__init__.py
+++ api/ping/__init__.py
@@ -2,15 +2,7 @@ import azure.functions as func
 from datetime import datetime
 from logging import info
 
-app = func.FunctionApp()
 
-
-@app.timer_trigger(
-    schedule="0 */5 * * * *",
-    arg_name="ping",
-    run_on_startup=False,
-    use_monitor=False,
-)
 def main(ping: func.TimerRequest) -> None:
     current_time = datetime.now()
     info(f"Pinged at {current_time}")
